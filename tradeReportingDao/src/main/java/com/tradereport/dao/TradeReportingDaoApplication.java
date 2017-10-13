5a359feed3f3299df82dab2cb5948d5205bc9d7b
nagarmathas <ameyanagarmath@yahoo.com>
2017-10-13 12:01:36 +0530

    initial setup of spring data jpa project for trade reporting dao

---
+++ tradeReportingDao/src/main/java/com/tradereport/dao/TradeReportingDaoApplication.java
@@ -0,0 +1,12 @@
+package com.tradereport.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeReportingDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeReportingDaoApplication.class, args);
+	}
+}
